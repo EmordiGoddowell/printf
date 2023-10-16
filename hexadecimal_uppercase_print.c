86ea61d5b0b3346810e2ba51e08a6c4d2d6f055b
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-10-15 18:56:45 -0700

    hexadecimal_uppercase_print

---
+++ hexadecimal_uppercase_print.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * hexadecimal_uppercase_print - Prints an unsigned
+ *		integer in uppercase hexadecimal format
+ * @l: A va_list that contains the arguments passed to the function
+ * @f: A pointer to a flags_t struct that
+ *		determines if a flag was passed to the function
+ *
+ * Return:  the number of characters printed
+**/
+
+int hexadecimal_uppercase_print(va_list l, flags_t *f)
+{
+	unsigned int num;
+	char *str;
+	int count;
+
+	num = va_arg(l, unsigned int);
+	str = converter(num, 16, 0);
+	count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _puts("0x");
+	count += _puts(str);
+
+	return (count);
+}
