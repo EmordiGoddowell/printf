d67b0c3566cf6e7207f03691b2e0dcd3a162fa30
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-10-15 18:57:52 -0700

    hexadecimal_print

---
+++ hexadecimal_print.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * hexadecimal_print - Prints an unsigned integer in
+ *			lowercase hexadecimal format
+ * @l: A va_list that contains the arguments passed to the function
+ * @f: A pointer to the flags_t struct that
+ *	determines if a flag was passed to the function
+ *
+ * Return: the number of characters printed
+**/
+
+int hexadecimal_print(va_list l, flags_t *f)
+{
+	unsigned int num;
+	char *str;
+	int count;
+
+	num = va_arg(l, unsigned int);
+	str = converter(num, 16, 1);
+	count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _puts("0x");
+	count += _puts(str);
+
+	return (count);
+}
