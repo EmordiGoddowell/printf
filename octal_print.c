ff6807b9b6362064d7e41756694acd5da892410f
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-10-16 04:20:06 -0700

    octal_print

---
+++ octal_print.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * octal_print - Prints an unsigned integer in octal format
+ * @l: A va_list that contains the arguments passed to the function
+ * @f: A pointer to a flags_t struct that
+ *	determines if a flag was passed to the function
+ *
+ * Return: the number of characters printed
+**/
+
+int octal_print(va_list l, flags_t *f)
+{
+	unsigned int num;
+	char *str;
+	int count;
+
+	num = va_arg(l, unsigned int);
+	str = converter(num, 8, 0);
+	count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _putchar('0');
+	count += _puts(str);
+
+	return (count);
+}
