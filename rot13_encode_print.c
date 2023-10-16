5c4adce1d49f335acaa08f95e36fe8b4a6c07513
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-10-16 04:21:32 -0700

    rot13_encode_print

---
+++ rot13_encode_print.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * rot13_encode_print - Prints a string in ROT13 format
+ * @l: A va_list that contains the arguments passed to the function
+ * @f: A pointer to a flags_t struct that
+ *	determines if a flag was passed to the function
+ *
+ * Return: the number of characters printed
+**/
+
+int rot13_encode_print(va_list l, flags_t *f)
+{
+	int i;
+	int j;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *s;
+
+	s = va_arg(l, char *);
+
+	(void)f;
+	for (j = 0; s[j]; j++)
+	{
+		if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
+			_putchar(s[j]);
+		else
+		{
+			for (i = 0; i <= 52; i++)
+			{
+				if (s[j] == rot13[i])
+					_putchar(ROT13[i]);
+			}
+		}
+	}
+
+	return (j);
+}
